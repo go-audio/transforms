51417e5db2ec4758d9472909b352da22d2edeed8
Matt Aimonetti <mattaimonetti@gmail.com>
2017-01-03 18:50:41 -0800

    documentation

--- gain.go
+++ gain.go
@@ -5,6 +5,8 @@ import "github.com/go-audio/audio"
 // Gain applies the multiplier to the passed buffer.
 // A multipler of 1 would increase the amplitude of the signal by 100%
 // while a multiplier of 0 would not do anything.
+// Note that this is a very very naive implementation and we will more
+// than add a more useful DB gain transform.
 func Gain(buf *audio.FloatBuffer, multiplier float64) error {
 	if buf == nil {
 		return audio.ErrInvalidBuffer
