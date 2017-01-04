608de82733a1bc66595f347287870727bd26621e
Matt Aimonetti <mattaimonetti@gmail.com>
2017-01-04 00:40:16 -0800

    update to support the updated audio buffer format

--- pcm_scale.go
+++ pcm_scale.go
@@ -3,13 +3,13 @@ package transforms
 import "github.com/go-audio/audio"
 
 // PCMScale converts a buffer with audio content from -1 to 1 into
-// the PCM scale based on the buffer's bitdepth.
+// the PCM scale based on the passed bitdepth.
 // Note that while the PCM data is scaled, the PCM format is not changed.
-func PCMScale(buf *audio.FloatBuffer) error {
+func PCMScale(buf *audio.FloatBuffer, bitDepth int) error {
 	if buf == nil || buf.Format == nil {
 		return audio.ErrInvalidBuffer
 	}
-	factor := float64(audio.IntMaxSignedValue(buf.Format.BitDepth))
+	factor := float64(audio.IntMaxSignedValue(bitDepth))
 	for i := 0; i < len(buf.Data); i++ {
 		buf.Data[i] *= factor
 	}
